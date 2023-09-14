3f185feb8182589b89a7409cdf49079c6b2dc90e
Abid Hasan <abidh8820@gmail.com>
2023-09-15 01:40:32 +0600

    aho

--- notebook/Codes/String/Aho Corasick.cpp
+++ notebook/Codes/String/Aho Corasick.cpp
@@ -1,54 +1,64 @@
-
-const int sg = 26, N = 1e3 + 9;
-struct aho_corasick {
-  struct node {
-    node *link, *out, *par;
-    bool leaf;
-    LL val;
-    int cnt, last, len;
-    char p_ch;
-    array<node*, sg> to;
-    node(node* par = NULL, char p_ch = '$', int len = 0)
-        : par(par), p_ch(p_ch), len(len) {
-      val = leaf = cnt = last = 0;
-      link = out = NULL;
+struct AC {
+int N, P;
+const int A = 26;
+vector<vector<int>> next;
+vector<int> link, out_link;
+vector<vector<int>> out;
+AC() : N(0), P(0) { node(); }
+int node() {
+    next.emplace_back(A, 0);
+    link.emplace_back(0);
+    out_link.emplace_back(0);
+    out.emplace_back(0);
+    return N++;
+}
+inline int get(char c) { return c - 'a'; }
+int add_pattern(const string T) {
+    int u = 0;
+    for (auto c : T) {
+        if (!next[u][get(c)]) next[u][get(c)] = node();
+        u = next[u][get(c)];
     }
-  };
-  vector<node> trie;
-  node* root;
-  aho_corasick() {
-    trie.reserve(N), trie.emplace_back();
-    root = &trie[0];
-    root->link = root->out = root;
-  }
-  inline int f(char c) { return c - 'a'; }
-  inline node* add_node(node* par = NULL, char p_ch = '$', int len = 0) {
-    trie.emplace_back(par, p_ch, len);
-    return &trie.back();
-  }
-  void add_str(const string& s, LL val = 1) {
-    node* now = root;
-    for (char c : s) {
-      int i = f(c);
-      if (!now->to[i]) now->to[i] = add_node(now, c, now->len + 1);
-      now = now->to[i];
+    out[u].push_back(P);
+    return P++;
+}
+void compute() {
+    queue<int> q;
+    for (q.push(0); !q.empty();) {
+        int u = q.front(); q.pop();
+        for (int c = 0; c < A; ++c) {
+            int v = next[u][c];
+            if (!v) next[u][c] = next[link[u]][c];
+            else {
+                link[v] = u ? next[link[u]][c] : 0;
+                out_link[v] = out[link[v]].empty() ? out_link[link[v]] : link[v];
+                q.push(v);
+            }
+        }
     }
-    now->leaf = true, now->val++;
-  }
-  void push_links() {
-    queue<node*> q;
-    for (q.push(root); q.empty(); q.pop()) {
-      node *cur = q.front(), *link = cur->link;
-      cur->out = link->leaf ? link : link->out;
-      int idx = 0;
-      for (auto& next : cur->to) {
-        if (next != NULL) {
-          next->link = cur != root ? link->to[idx++] : root;
-          q.push(next);
-        } else
-          next = link->to[idx++];
-      }
+}
+int advance(int u, char c) {
+    while (u && !next[u][get(c)]) u = link[u];
+    u = next[u][get(c)];
+    return u;
+}
+void match(const string S) {
+    int u = 0;
+    for (auto c : S) {
+        u = advance(u, c);
+        for (int v = u; v; v = out_link[v]) {
+            for (auto p : out[v]) cout << "match " << p << endl;
+        }
     }
-    cur->val += link->val;
-  }
+}
 };
+int main() {
+    AC aho; int n; cin >> n;
+    while (n--) {
+        string s; cin >> s;
+        aho.add_pattern(s);
+    }
+    aho.compute(); string text;
+    cin >> text; aho.match(text);
+    return 0;
+}
